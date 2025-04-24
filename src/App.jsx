4d5eeee10e72d38319aea034cd07df7f4ea1b27a
luis <ecluisfilipe@gmail.com>
2025-04-24 20:46:53 -0300

    first comitt

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Historia from './Historia'; // Caminho correto
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Historia />
+      </main>
+    </div>
+  );
+}
+
+export default App;
